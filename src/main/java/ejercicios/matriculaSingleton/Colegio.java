a16d124e44352f855afb5cbba5014fad2e6d1a79
NickyGon <60781642+NickyGon@users.noreply.github.com>
2021-05-31 09:22:52 -0400

    Ejercicio de singleton (Se salto el singleton el push anterior)

---
+++ src/main/java/ejercicios/matriculaSingleton/Colegio.java
@@ -0,0 +1,31 @@
+package ejercicios.matriculaSingleton;
+
+public class Colegio {
+    private RH rh;
+    private String nombreColegio;
+
+    public Colegio() {
+        this.rh = rh;
+        this.nombreColegio = nombreColegio;
+    }
+
+    public RH getRh() {
+        return rh;
+    }
+
+    public void setRh(RH rh) {
+        this.rh = rh;
+    }
+
+    public String getNombreColegio() {
+        return nombreColegio;
+    }
+
+    public void setNombreColegio(String nombreColegio) {
+        this.nombreColegio = nombreColegio;
+    }
+
+    public void pagarMatricula(int amount,String hora, Estudiante estudiante){
+        Ventanilla.;
+    }
+}
---
+++ src/main/java/ejercicios/matriculaSingleton/RH.java
@@ -0,0 +1,27 @@
+package ejercicios.matriculaSingleton;
+
+public class RH {
+    private String ci;
+    private String nombre;
+
+    public RH(String ci, String nombre) {
+        this.ci = ci;
+        this.nombre = nombre;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
